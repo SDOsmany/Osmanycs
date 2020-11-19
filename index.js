fa553d4ebc5cb5c7d3c8828aaa68e4aa8c248590
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2020-11-19 17:15:00 -0500

    Resume website and side menu changes

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-/* Set the width of the side navigation to 250px */
+//SIDE NAV BAR----------------------------------------------------------------------------
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("large-header").style.marginLeft = "250px";
+    document.getElementById("MyBurger").style.display = "none";
   }
   
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("large-header").style.marginLeft = "0";
+    document.getElementById("MyBurger").style.display = "block";
   }
+  //ENDSIDE NAV BAR----------------------------------------------------------------------------
