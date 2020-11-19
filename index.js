bf70f0515ba1b25ef53fd332d8a5cd4e52b12d6b
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2020-11-19 18:02:12 -0500

    Working on the about me link

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ function openNav() {
     document.getElementById("MyBurger").style.display = "block";
   }
   //ENDSIDE NAV BAR----------------------------------------------------------------------------
+let aboutMe = ()=>{
+  document.getElementById("AboutMe").style.display="block";
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("large-header").style.marginLeft = "0";
+  document.getElementById("MyBurger").style.display = "block";
+}
+let returnHome= ()=>{
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("large-header").style.marginLeft = "0";
+  document.getElementById("MyBurger").style.display = "block";
+  document.getElementById("AboutMe").style.display="none";
+}
